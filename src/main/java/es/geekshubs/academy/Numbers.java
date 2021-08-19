ca1f4fd1ace76270e966ece24891a725c072b1ba
smjcarlos96 <smjcarlos96@gmail.com>
2021-08-19 18:29:09 +0200

    Solution

--- src/main/java/es/geekshubs/academy/Numbers.java
+++ src/main/java/es/geekshubs/academy/Numbers.java
@@ -7,13 +7,33 @@ import java.util.stream.Collectors;
 
 public class Numbers {
 
-  /**
-   * Apply Method
-   * @param input
-   * @return
-   */
-  public List<String> apply(final List<Integer> input) {
-    return null;
-  }
+	/**
+	 * Apply Method
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public List<String> apply(final List<Integer> input) {
+		List<String> output = new ArrayList<String>();
+		int nPos = 0,
+			nNeg = 0,
+			nZro = 0;
+		
+		for(Integer n : input) {
+			if(n>0)
+				nPos++;
+			else
+				if(n==0)
+					nZro++;
+				else
+					nNeg++;
+		}
+		
+		output.add(String.format("%.4f", nPos/input.size()));
+		output.add(String.format("%.4f", nNeg/input.size()));
+		output.add(String.format("%.4f", nZro/input.size()));
+		
+		return output;
+	}
 
 }
